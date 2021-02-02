9854112c3d947ec5b0dc7b472d16112c62079f83
กมลทิพย์ เทศทอง <60011212001@o365.msu.ac.th>
2021-02-02 11:39:41 +0700

    sidebar active childe route

--- src/components/Content/Sidebar.vue
+++ src/components/Content/Sidebar.vue
@@ -6,8 +6,18 @@
     <!-- <router-link to="/">Main</router-link> -->
     <div class="mt-4">
     <ul class="nav font-no-size-color">
-      <li><router-link :to="{name : 'MonitorList' }"><font-awesome-icon :icon="['fas', 'desktop']"/>  Monitor Error</router-link></li>
-      <li><router-link :to="{name : 'ListProject' }"><font-awesome-icon :icon="['far', 'check-square']"/>  Project Management</router-link></li>
+      <li><router-link :to="{name : 'MonitorList' }"
+        :class="{'router-link-active': $route.fullPath === '/monitor/list' || 
+          $route.fullPath === '/monitor/detail'}">
+          <font-awesome-icon :icon="['fas', 'desktop']"/>  Monitor Error
+        </router-link></li>
+      <li><router-link :to="{name : 'ListProject' }" 
+        :class="{'router-link-active': $route.fullPath === '/project/list' || 
+          $route.fullPath === '/project/detail' || 
+          $route.fullPath === '/project/add' || 
+          $route.fullPath === '/project/edit'}">
+          <font-awesome-icon :icon="['far', 'check-square']"/>  Project Management
+        </router-link></li>
       <li><router-link to="/page"><font-awesome-icon :icon="['fas', 'chart-pie']"/>  Dashboard</router-link></li>
       <li><router-link to="/page"><font-awesome-icon :icon="['fas', 'code']"/>  Document API</router-link></li>
     </ul>
@@ -26,6 +36,6 @@ export default {
 };
 </script>
 
-
+<style>
 
 </style>
