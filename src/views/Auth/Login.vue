8fea67be5188fa775d48067540419a0960a25563
ณัฐวุฒิ ชัยพิมล <60011212153@o365.msu.ac.th>
2021-02-03 15:10:43 +0700

    edit task

--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -1,61 +1,70 @@
 <template>
-  <div id="login" class="background p-5 ">
-    <div class="container background-container rounded ">
-      <div class="row ">
-        <div class="col image-stlay">
-          <img src="/img/image1.png" style="height: 100%; width: 100%;"/>
-        </div>
-        <div class="col pr-5">
-          <h1 class="title-font mt-5 mb-5">Sing In</h1>
-          <b-form-input
-            v-model="text"
-            placeholder="Username"
-            class="rounded-pill mt-3"
-          ></b-form-input>
-          <b-form-input
-            v-model="text"
-            placeholder="Password"
-            class="rounded-pill mt-3"
-          ></b-form-input>
-          <b-form-checkbox
-            class="mt-3 float-left"
-            id="checkbox-1"
-            v-model="status"
-            name="checkbox-1"
-            value="accepted"
-            unchecked-value="not_accepted"
-          >
-            Keep me signed in
-          </b-form-checkbox>
-          <b-button class="rounded-pill button-font" block variant="primary" @click="clickLogin()">
-            <table class="table-stlay">
-              <tr>
-                <td class="pl-5">Log In</td>
-                <td class="float-right">
-                  <font-awesome-icon
-                    :icon="['fas', 'arrow-right']"
-                  />
-                </td>
-              </tr>
-            </table>
-          </b-button>
-          <p class="mt-3 mb-5 forgot-font">Forgot password?</p>
+  
+    <div id="login" class="background p-5 h-80">
+      <div class="container background-container rounded ">
+        <div class="row ">
+          <div class="col image-stlay">
+            <img src="/img/image1.png" style="height: 100%; width: 100%;" />
+          </div>
+          <div class="col pr-5">
+            <h1 class="title-font text-center mt-5 mb-5">Sing In</h1>
+            <b-form-input
+              v-model="text"
+              placeholder="Username"
+              class="rounded-pill mt-3"
+            ></b-form-input>
+            <b-form-input
+              v-model="text"
+              placeholder="Password"
+              class="rounded-pill mt-3"
+            ></b-form-input>
+            <b-form-checkbox
+              class="mt-3 float-left"
+              id="checkbox-1"
+              v-model="status"
+              name="checkbox-1"
+              value="accepted"
+              unchecked-value="not_accepted"
+            >
+              Keep me signed in
+            </b-form-checkbox>
+            <b-button
+              class="rounded-pill button-font"
+              block
+              variant="primary"
+              @click="clickLogin()"
+            >
+              <table class="table-stlay">
+                <tr>
+                  <td class="pl-5">Log In</td>
+                  <td class="float-right">
+                    <font-awesome-icon :icon="['fas', 'arrow-right']" />
+                  </td>
+                </tr>
+              </table>
+            </b-button>
+            <p class="mt-3 mb-5 forgot-font">Forgot password?</p>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+ 
 </template>
 
 <script>
 export default {
   name: "login",
-  methods:{
-    clickLogin(){
-      this.$router.push({name : 'ListProject'});
+  methods: {
+    clickLogin() {
+      this.$router.push({ name: "ListProject" });
       //this.$store.dispatch("setAllLinkHeader", listlink);
-    }
+    },
   },
 };
 </script>
 
-<style></style>
+<style>
+.full-height {
+  height: 100%;
+}
+</style>
--- src/views/MonitorError/Task.vue
+++ src/views/MonitorError/Task.vue
@@ -1,16 +1,218 @@
 <template>
   <div>
-      <h1>55555555555555555</h1>
-      Add List  Fa
+    <div class="p-5 background-container">
+      <div class="row">
+        <div class="col-xl col-lg-12">
+          <ul class="p-0">
+            <li class="list-inline-item">
+              <p class="font-weight-bold">Project Name :</p>
+            </li>
+            <li class="list-inline-item ">
+              <p class="font-weight-light">Covid 19 Airports project</p>
+            </li>
+          </ul>
+          <ul class="p-0">
+            <li class="list-inline-item">
+              <p class="font-weight-bold">Project Owner :</p>
+            </li>
+            <li class="list-inline-item">
+              <p class="font-weight-light">Covid 19 Airports project</p>
+            </li>
+          </ul>
+          <ul class="p-0">
+            <li class="list-inline-item">
+              <p class="font-weight-bold">Service ID :</p>
+            </li>
+            <li class="list-inline-item">
+              <p class="font-weight-light">Covid 19 Airports project</p>
+            </li>
+          </ul>
+        </div>
+        <div class="col-xl-6 col-lg-12">
+          <ul class="p-0">
+            <li class="list-inline-item">
+              <p class="font-weight-bold">Service Name :</p>
+            </li>
+            <li class="list-inline-item">
+              <p class="font-weight-light">Covid 19 Airports project</p>
+            </li>
+          </ul>
+          <ul class=" p-0">
+            <li class="list-inline-item ">
+              <p class="font-weight-bold">User Maintenance :</p>
+            </li>
+            <li class="list-inline-item">
+              <p class="font-weight-light">Covid 19 Airports project</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="p-5 background-container mt-5">
+      <div class="row">
+        <div class="col-xl-4 col-lg-5 col-sm-8 align-middle">
+          <h5>สถานะ Service Error</h5>
+        </div>
+        <div class="col-xl-8 col-lg-7 col-sm-4">
+          <div class="d-flex justify-content-end">
+            <b-form-group v-slot="{ ariaDescribedby }" class="display-task">
+              <b-form-checkbox-group
+                
+                size="lg"
+                id="checkbox-group-1"
+                v-model="selected"
+                :options="options"
+                :aria-describedby="ariaDescribedby"
+                name="flavour-1"
+              ></b-form-checkbox-group>
+            </b-form-group>
+
+            <b-dropdown
+              text="Filter"
+              variant="primary"
+              class="m-2 display-dropdow-task"
+            >
+              <b-dropdown-form>
+                <b-form-group v-slot="{ ariaDescribedby }">
+                  <b-form-checkbox-group
+                    size="lg"
+                    id="checkbox-group-2"
+                    v-model="selected"
+                    :options="options"
+                    :aria-describedby="ariaDescribedby"
+                    name="flavour-2"
+                  ></b-form-checkbox-group>
+                </b-form-group>
+              </b-dropdown-form>
+            </b-dropdown>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div class="row">
+        <div
+          class="col-xl-3 col-lg-6"
+          v-for="(task, index) in listTasks"
+          :key="index"
+        >
+          <b-card class="card-list">
+            <b-card-title>{{ task.name }}</b-card-title>
+            <b-card-text>
+              {{ task.detail }}
+            </b-card-text>
+            <div class="d-flex justify-content-end">
+              <div class="status-waiting rounded-pill" v-if="task.status == 'waiting'">
+                <p class="fw-bold m-0 text-center white-space">
+                  Waiting
+                  <font-awesome-icon class="font-status"
+                    :icon="['fas', 'tag']"
+                  />
+                </p>
+              </div>
+              <div class="status-todo rounded-pill" v-if="task.status == 'todo'">
+                <p class="fw-bold m-0 text-center white-space">
+                  To do
+                  <font-awesome-icon class="font-status" :icon="['fas', 'tag']" />
+                </p>
+              </div>
+              <div class="status-doing rounded-pill" v-if="task.status == 'doing'">
+                <p class="fw-bold m-0 text-center white-space">
+                  Doing
+                  <font-awesome-icon class="font-status" :icon="['fas', 'tag']" />
+                </p>
+              </div>
+              <div class="status-testing rounded-pill" v-if="task.status == 'testing'">
+                <p class="fw-bold m-0 text-center white-space">
+                  Testing
+                  <font-awesome-icon class="font-status" :icon="['fas', 'tag']" />
+                </p>
+              </div>
+              <div class="status-done rounded-pill" v-if="task.status == 'done'">
+                <p class="fw-bold m-0 text-center white-space">
+                  Done
+                  <font-awesome-icon class="font-status" :icon="['fas', 'tag']" />
+                </p>
+              </div>
+            </div>
+          </b-card>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { filter } from 'vue/types/umd';
 export default {
-    name :'TaskError'
-}
+  name: "TaskError",
+  methods:{
+    
+  },
+  data() {
+    return {
+      selected: [], // Must be an array reference!
+      options: [
+        { text: "Waiting", value: "waiting" },
+        { text: "To do", value: "todo" },
+        { text: "Doing", value: "doing" },
+        { text: "Testing", value: "testing" },
+        { text: "Done", value: "done" },
+      ],
+      listTasks: [
+        {
+          name: "ERROR1",
+          detail: "Detail Error 1 Connect not sucess",
+          status: "waiting",
+        },
+        {
+          name: "ERROR2",
+          detail: "Detail Error 1 Connect not sucess",
+          status: "waiting",
+        },
+        {
+          name: "ERROR1",
+          detail: "Detail Error 1 Connect not sucess",
+          status: "waiting",
+        },
+        {
+          name: "ERROR1",
+          detail: "Detail Error 1 Connect not sucess",
+          status: "done",
+        },
+        {
+          name: "ERROR1",
+          detail: "Detail Error 1 Connect not sucess",
+          status: "testing",
+        },
+        {
+          name: "ERROR1",
+          detail: "Detail Error 1 Connect not sucess",
+          status: "todo",
+        },
+        {
+          name: "ERROR1",
+          detail: "Detail Error 1 Connect not sucess",
+          status: "doing",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-
+.display-task {
+  display: block;
+}
+.display-dropdow-task {
+  display: none;
+}
+@media (max-width: 992px) {
+  .display-task {
+    display: none;
+  }
+  .display-dropdow-task {
+    display: block;
+  }
+}
 </style>
