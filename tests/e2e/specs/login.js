13171dfcf01f01791b13e9bd0ac963aa4507ec78
unzenXD <61413158+unzenXD@users.noreply.github.com>
2021-03-01 10:36:26 +0700

    poom

--- tests/e2e/specs/login.js
+++ tests/e2e/specs/login.js
@@ -28,56 +28,34 @@ describe("My First Test", () => {
 
    });
 
-   it("Home", () => {
-
-  // cy.visit('http://localhost:8080/login')
-  // cy.get('#username').type('GameKanna')
-  // cy.get('#password').type('Bb@2541')
-  // cy.get('#checkbox-1').check({force: true})
-  // cy.get('.pl-5').click()
-  // // cy.visit('http://localhost:8080/home/monitor/list')
-
-  // cy.reload().wait(2000)
-
-  // cy.get('.input-search').type('project 1')
-  // cy.get('#project-2').should('not.exist')
-
-  // cy.get('.input-search').clear().type('2')
-  // cy.get('#project-1').should('not.exist')
-  // cy.get('#project-2').should('not.exist')
-  // cy.get('#project-0').click()
-
-  // cy.get('.navigation-icon').should('not.visible')
-  // cy.viewport(1000, 660).wait(1000)
-  // cy.get('.navigation-icon').should('be.visible').click().click()
-  // cy.get('.row > .col-xl > .d-flex > .p-2 > a:nth-child(2)').click()
-
-
-   });
-
   it("Error", () => {
 
     cy.visit('http://localhost:8080/login')
     cy.get('#username').type('GameKanna')
     cy.get('#password').type('Bb@2541')
     cy.get('#checkbox-1').check({force: true})
-    cy.get('.pl-5').click()
+    cy.get('.pl-5').click().wait(1000)
     cy.visit('http://localhost:8080/home/monitor/list')
   
     cy.reload().wait(2000)
   
     cy.get('.input-search').type('project 1')
-    cy.get('#project-2').should('not.exist')
   
-    cy.get('.input-search').clear().type('2')
+    cy.get('.input-search').clear().type('4')
     cy.get('#project-1').should('not.exist')
     cy.get('#project-2').should('not.exist')
     cy.get('#project-0').click()
   
     cy.get('.navigation-icon').should('not.visible')
-    cy.viewport(1000, 660).wait(1000)
-    cy.get('.navigation-icon').should('be.visible').click().click()
-    cy.get('.row > .col-xl > .d-flex > .p-2 > a:nth-child(2)').click()
+    // cy.viewport(1000, 660).wait(1000)
+    // cy.get('.navigation-icon').should('be.visible').click().click()
+
+    // cy.viewport(1440, 660)
+
+    cy.get('#1').wait(1000).click()
+    cy.get('.row > .col-xl > .d-flex > .p-2 > a:nth-child(2)').first().click()
+   
+    
   
   
     });
